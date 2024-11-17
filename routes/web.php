3c959debc7db09bcdea7b71b7a43085032ed9fa3
Rayvinhuang <rayvinsuhartoyo_2226250007@mhs.mdp.ac.id>
2024-11-17 21:01:16 +0700

    first Commit

--- routes/web.php
+++ routes/web.php
@@ -21,9 +21,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+     return view('welcome');
+ });
 
 Route::middleware(['auth', 'checkRole:A,U'])->group(function () {
     Route::get('dashboard', [HomeController::class, 'index'])->middleware(['auth', 'verified', 'checkRole:A,U'])->name('dashboard');
